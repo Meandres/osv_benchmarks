f95515ecabd0b673d9441846e95b59a0d6e6223e
Martin Lindbüchl <lblsolutions@outlook.de>
2024-11-19 10:10:58 +0000

    refactored benchmarks

---
+++ help/help.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "OSV memory allocator benchmark tool. Run ./scripts/run.py -e '/<benchmark>'" << std::endl;
+  return 0;
+}
---
+++ multichase/module.py
@@ -0,0 +1,7 @@
+from osv.modules import api
+
+fairness = api.run("/fairness")
+gen_expand = api.run("/gen_expand")
+multichase = api.run("/multichase")
+multiload = api.run("/multiload")
+pingpong = api.run("/poingpong")
