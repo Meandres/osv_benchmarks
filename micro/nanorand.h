24498fb9ec2c2e492fdab50301d0f46f32b6e5e8
Martin Lindbüchl <lblsolutions@outlook.de>
2024-11-28 15:35:28 +0000

    add random.cpp

--- micro/nanorand.h
+++ micro/nanorand.h
@@ -1,24 +1,24 @@
-// Modified version of https://github.com/luhsra/linux-alloc-bench.git/nanorand.h
-
-
-#include "osv/types.h"
+// Modified version of
+// https://github.com/luhsra/linux-alloc-bench.git/nanorand.h
 
 /// Generates new random number modifying the seed for the next call.
 ///
 /// @see
 /// - https://github.com/wangyi-fudan/wyhash
 /// - https://github.com/Absolucy/nanorand-rs
-inline u64 nanorand_random(u64 *seed) {
+#include <cstdint>
+inline uint64_t nanorand_random(uint64_t *seed) {
   __uint128_t t;
   *seed = *seed + 0xa0761d6478bd642full;
   t = ((__uint128_t)*seed) * ((__uint128_t)(*seed ^ 0xe7037ed1a0b428dbull));
-  return (u64)((t >> 64) ^ t);
+  return (uint64_t)((t >> 64) ^ t);
 }
 
 /// Generates new random number in this range, modifying the seed for the next
 /// call.
-inline u64 nanorand_random_range(u64 *seed, u64 start, u64 end) {
-  u64 val = nanorand_random(seed);
+inline uint64_t nanorand_random_range(uint64_t *seed, uint64_t start,
+                                      uint64_t end) {
+  uint64_t val = nanorand_random(seed);
   val %= end - start;
   val += start;
   return val;
--- micro/random.cpp
+++ micro/random.cpp
@@ -8,39 +8,60 @@
 #include <osv/pagealloc.hh>
 #include <thread>
 
+#define PART_REALLOCATIONS 5
+#define ITER_REALLOCATION 100
+
 namespace benchmark {
 
-// Alloc and free the same page
-void repeat_worker(const size_t allocations, uint64_t *res) {
-  uint64_t start = rdtsc();
-  for (size_t i = 0; i < allocations; i++) {
+// Alloc and free pages at random
+void random_worker(const size_t allocations, void *const pages, uint64_t *res) {
+  uint64_t start_free = rdtsc();
+  for (size_t i = 0; i < allocations / 10; i++) {
     void *page = memory::physically_alloc_page();
     memory::physically_free_page(page);
   }
-  uint64_t end = rdtsc();
+  uint64_t end_free = rdtsc();
 
-  *res = end - start;
+  *res = end_free - start_free;
 }
 
 } // namespace benchmark
 
+// allocate operations*thread pages before
+void populate_pages(u64 const page_len, void **const pages) {
+  for (size_t k = 0; k < page_len; k++) {
+    pages[k] = memory::physically_alloc_page();
+  }
+}
+
+// deterministically determine which pages to free
+void populate_to_free(size_t const free_len, u64 const page_len, u64 *seed,
+                      u64 *const to_free) {
+  for (size_t i = 0; i < free_len; i++) {
+    u64 entry = nanorand_random_range(seed, 0, page_len);
+    for (size_t k = 0; k < i; k++) {
+      if (to_free[k] == entry)
+        return populate_to_free(free_len - i, page_len, seed, to_free + i);
+    }
+    to_free[i] = entry;
+  }
+}
+
 int main(int argc, char *argv[]) {
-  size_t allocations = 50000;
+  size_t operations = 50000;
   size_t iterations = 10;
   size_t threads = 1;
-  u64 seed = 0;
+  u64 seed = 5;
   std::ostream *output = &std::cout; // Default to stdout
   std::ofstream fileOutput;
 
   for (int i = 1; i < argc; ++i) {
     if (strcmp(argv[i], "-a") == 0 && i + 1 < argc) {
-      allocations = std::strtoul(argv[++i], nullptr, 10);
+      operations = std::strtoul(argv[++i], nullptr, 10);
     } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
       iterations = std::strtoul(argv[++i], nullptr, 10);
     } else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
       threads = std::strtoul(argv[++i], nullptr, 10);
-    } else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
-      seed = std::strtoul(argv[++i], nullptr, 10);
     } else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
       fileOutput.open(argv[++i]);
       if (!fileOutput.is_open()) {
@@ -57,19 +78,31 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  (*output) << "operations " << allocations << "\n";
+  (*output) << "operations " << operations << "\n";
   (*output) << "threads    " << threads << "\n";
   (*output) << "iterations " << iterations << "\n";
-  (*output) << "[iteration][thread] ticks\n";
+  (*output) << "[iteration][thread] ticks ticks\n";
 
   uint64_t it[iterations][threads];
   uint64_t avg = 0;
 
+  u64 const page_len = operations * threads;
+  u64 const free_len = page_len / PART_REALLOCATIONS;
+
+  void *pages[page_len];
+  u64 to_free[free_len];
+
   for (size_t i = 0; i < iterations; i++) {
     std::thread thread_pool[threads];
+    populate_pages(page_len, pages);
+    populate_to_free(free_len, page_len, &seed, to_free);
     for (size_t t = 0; t < threads; t++) {
-      thread_pool[t] =
-          std::thread(benchmark::repeat_worker, allocations, it[i] + t);
+      thread_pool[t] = std::thread(benchmark::random_worker, operations,
+                                   pages + t, it[i] + t);
+    }
+    // free operations*threads pages after
+    for (size_t k = 0; k < operations * threads; k++) {
+      memory::physically_free_page(pages[k]);
     }
 
     for (size_t t = 0; t < threads; t++) {
@@ -79,7 +112,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  (*output) << "per_operation:   " << avg / iterations / threads / allocations
+  (*output) << "per_operation:   " << avg / iterations / threads / operations
             << " Ticks\n";
 
   if (fileOutput.is_open()) {
