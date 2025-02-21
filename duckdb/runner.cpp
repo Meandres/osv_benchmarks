789e99479c8e6032ecb4dd9361e6239216186206
Martin Lindbüchl <lblsolutions@outlook.de>
2025-02-21 09:55:32 +0000

    minor changes and runner configuration

--- duckdb/runner.cpp
+++ duckdb/runner.cpp
@@ -1,13 +1,18 @@
-#include "duckdb/benchmark//include//benchmark_runner.hpp"
+#include "duckdb/benchmark/include//benchmark_runner.hpp"
 
 int main()
 {
     const char* args[] = {
         "benchmark_runner", 
-        "benchmark/tpch/expression_reordering/.*", 
-        nullptr  // Null-terminated array
+        "--nruns=2,6,1",
+        "--idle_time=2000,0,0",
+        "--transition_time=0,1000",
+        "benchmark/tpch/expression_reordering/adaptive_mixed_reordering_and.benchmark",
+        "benchmark/tpch/expression_reordering/adaptive_string_reordering_or.benchmark",
+        "benchmark/tpch/expression_reordering/adaptive_mixed_reordering_or.benchmark",
+        0
     };
 
-    duckdb::setup(2, const_cast<char**>(args));
+    duckdb::setup(std::size(args) - 1, const_cast<char**>(args));
     duckdb::run();
 }
