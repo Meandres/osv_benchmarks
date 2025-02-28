08093172e632d84a9ef9f91adbc7c96cd41cb7ec
Martin Lindbüchl <lblsolutions@outlook.de>
2025-02-28 10:11:17 +0000

    generate tpc db

---
+++ duckdb/tpch-queries.sql
@@ -0,0 +1,27 @@
+PRAGMA version;
+SET enable_progress_bar = false;
+LOAD tpch;
+CALL dbgen(sf = 100);
+.timer on
+PRAGMA tpch(1);
+PRAGMA tpch(2);
+PRAGMA tpch(3);
+PRAGMA tpch(4);
+PRAGMA tpch(5);
+PRAGMA tpch(6);
+PRAGMA tpch(7);
+PRAGMA tpch(8);
+PRAGMA tpch(9);
+PRAGMA tpch(10);
+PRAGMA tpch(11);
+PRAGMA tpch(12);
+PRAGMA tpch(13);
+PRAGMA tpch(14);
+PRAGMA tpch(15);
+PRAGMA tpch(16);
+PRAGMA tpch(17);
+PRAGMA tpch(18);
+PRAGMA tpch(19);
+PRAGMA tpch(20);
+PRAGMA tpch(21);
+PRAGMA tpch(22);
