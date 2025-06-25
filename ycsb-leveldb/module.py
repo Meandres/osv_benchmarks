d974fbd4beeef642cf98322f41f9df2864747a50
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2025-06-25 13:28:08 +0000

    fixing default run

--- ycsb-leveldb/module.py
+++ ycsb-leveldb/module.py
@@ -1,3 +1,3 @@
 from osv.modules import api
 
-default = api.run('/ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s')
+default = api.run('/ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -p threadcount=4 -p recordcount=10000000 -p leveldb.cache_size=134217728 -s')
